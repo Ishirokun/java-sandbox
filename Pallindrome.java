74607e9ee6f42af505e01b20e2086637331f42d8
Ishiro-kun <33931157+Ishirokun@users.noreply.github.com>
2024-07-12 13:05:02 +0000

    shortened

--- Pallindrome.java
+++ Pallindrome.java
@@ -1,48 +1,10 @@
 import java.util.Scanner;
 
 public class Pallindrome {
-
-    /**
-     * Trims and removes common punctuation marks, symbols and spaces.
-     * 
-     * List of removed characters:
-     * ['.', ',', '!', ';', ':', '&', ' ']
-     * 
-     * @param string
-     * @return result
-     */
-    public static String isolateLetters(String string){
-        String result;
-        result = string.trim();
-        result = result.replace(".", "");
-        result = result.replace(",", "");
-        result = result.replace("!", "");
-        result = result.replace(";", "");
-        result = result.replace(":", "");
-        result = result.replace("&", "");
-        result = result.replace(" ", "");
-        return result;
-    }
-
     public static void main(String[] args) {
-        String original, converted;
-        Scanner input = new Scanner(System.in);
         System.out.println("Enter a string ");
-        // This function captures the next string typed into the terminal.
-        original = input.nextLine();
-
-        // isolateLetters functions above
-        converted = isolateLetters(original);
-        StringBuilder reverse = new StringBuilder(converted);
-        reverse.reverse();
-
-        // Ignores capitalization and compares the word into it's reverse, effectively detecting a Pallindrome.
-        if(converted.equalsIgnoreCase(reverse.toString())){
-            System.out.println("Pallindrome");
-        }else{
-            System.out.println("Not Pallindrome");
-        }
-        
+        String word = new Scanner(System.in).nextLine().replaceAll("[,\\s!&;:]", "");
+        if (word.equalsIgnoreCase(new StringBuilder(word).reverse().toString())) System.out.println("Pallindrome");
+        else System.out.println("Not Pallindrome");
     }
-
 }
