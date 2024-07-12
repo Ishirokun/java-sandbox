c25f3ee10072610b7038dc4eae8fac5714124458
Ishiro-kun <33931157+Ishirokun@users.noreply.github.com>
2024-07-12 12:49:17 +0000

    added pallindrome class

---
+++ Pallindrome.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Pallindrome {
+
+    /**
+     * Removes common punctuation marks, symbols and spaces.
+     * 
+     * List of removed characters:
+     * ['.', ',', '!', ';', ':', '&', ' ']
+     * 
+     * @param string
+     * @return result
+     */
+    public static String isolateLetters(String string){
+        String result;
+        result = string.replace(".", "");
+        result = result.replace(",", "");
+        result = result.replace("!", "");
+        result = result.replace(";", "");
+        result = result.replace(":", "");
+        result = result.replace("&", "");
+        result = result.replace(" ", "");
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String original, converted;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string ");
+        // This function captures the next string typed into the terminal.
+        original = input.nextLine();
+
+        // isolateLetters functions above
+        converted = isolateLetters(original);
+        StringBuilder reverse = new StringBuilder(converted);
+        reverse.reverse();
+
+        // Ignores capitalization and compares the word into it's reverse, effectively detecting a Pallindrome.
+        if(converted.equalsIgnoreCase(reverse.toString())){
+            System.out.println("Pallindrome");
+        }else{
+            System.out.println("Not Pallindrome");
+        }
+        
+    }
+
+}
